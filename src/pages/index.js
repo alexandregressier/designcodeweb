5914f0384d39a16c268ac956f424e9b56dc90c2f
Alexandre Gressier <alexandre@gressier.dev>
2021-10-24 23:47:28 +0200

    Set the title & subtitle of the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
 
-function IndexPage() {
-  return (
+const IndexPage = () =>
     <Layout>
-      <SEO title="Home" />
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+        <SEO title="Home"/>
+        <img src="/images/logos/logo.svg" alt="logo"/>
+        <h1>Design and code React apps</h1>
+        <p>
+            Don’t skip design. Learn design and code, by building real apps with React
+            and Swift. Complete courses about the best tools.
+        </p>
+        <Link to="/page-2/">Go to page 2</Link> <br/>
     </Layout>
-  )
-}
 
 export default IndexPage
