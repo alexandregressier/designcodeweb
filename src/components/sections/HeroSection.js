501b9d12be00ac85bdd38f42db65682748e34786
Alexandre Gressier <alexandre@gressier.dev>
2021-10-26 12:19:06 +0200

    Define the structure of a section & style it

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,16 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
 
 const HeroSection = () =>
-    <>
-        <img src="/images/logos/logo.svg" alt="logo"/>
-        <h1>Design and code React apps</h1>
-        <p>
-            Don’t skip design. Learn design and code, by building real apps with React
-            and Swift. Complete courses about the best tools.
-        </p>
-        <Link to="/page-2/">Go to page 2</Link>
-        <br/>
-    </>
+    <Wrapper>
+        <ContentWrapper>
+            <TextWrapper>
+                <Title>Design<br/>and code React apps</Title>
+                <Description>
+                    Don’t skip design. Learn design and code, by building real apps with
+                    React and Swift. Complete courses about the best tools.
+                </Description>
+            </TextWrapper>
+        </ContentWrapper>
+    </Wrapper>
 
 export default HeroSection
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
+`
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`
+const Description = styled.div`
+  font-size: 17px;
+  line-height: 130%;
+`
