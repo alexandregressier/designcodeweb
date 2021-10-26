9a741aba500672e6ca3af30c1e0f0ec4ef5eccd6
Alexandre Gressier <alexandre@gressier.dev>
2021-10-26 11:07:03 +0200

    Create the HeroSection component

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const HeroSection = () =>
+    <>
+        <img src="/images/logos/logo.svg" alt="logo"/>
+        <h1>Design and code React apps</h1>
+        <p>
+            Don’t skip design. Learn design and code, by building real apps with React
+            and Swift. Complete courses about the best tools.
+        </p>
+        <Link to="/page-2/">Go to page 2</Link>
+        <br/>
+    </>
+
+export default HeroSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
+import HeroSection from "../components/sections/HeroSection"
 
 const IndexPage = () =>
     <Layout>
         <SEO title="Home"/>
-        <img src="/images/logos/logo.svg" alt="logo"/>
-        <h1>Design and code React apps</h1>
-        <p>
-            Don’t skip design. Learn design and code, by building real apps with React
-            and Swift. Complete courses about the best tools.
-        </p>
-        <Link to="/page-2/">Go to page 2</Link> <br/>
+        <HeroSection/>
     </Layout>
 
 export default IndexPage
